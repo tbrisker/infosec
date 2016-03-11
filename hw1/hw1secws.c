c25e8c3b145a3467f62bed5ac8adcc06f4d8662e
Tomer Brisker <tbrisker@gmail.com>
2016-03-11 19:09:39 +0200

    cleanup

--- hw1/hw1secws.c
+++ hw1/hw1secws.c
@@ -8,16 +8,17 @@ MODULE_AUTHOR("Tomer Brisker");
 
 #define NUM_HOOKS 3
 
+/* Array to hold our hook definitions so we can easily register and unregister them */
 static struct nf_hook_ops hooks[NUM_HOOKS];
 
-/* the main firewall logic lies here */
+/* the main firewall logic - this function decide what packets are blocked and which are allowed */
 static unsigned int firewall(unsigned int hooknum,
-                        struct sk_buff *skb,
-                        const struct net_device *in,
-                        const struct net_device *out,
-                        int (*okfn)(struct sk_buff *)){
+                             struct sk_buff *skb,
+                             const struct net_device *in,
+                             const struct net_device *out,
+                             int (*okfn)(struct sk_buff *)){
 #ifdef DEBUG
-    printk(KERN_INFO "firewall triggered, hooknum: %d, in: %s, out: %s\n",
+    printk(KERN_DEBUG "firewall triggered, hooknum: %d, in: %s, out: %s\n",
             hooknum, in ? in->name : "none", out ? out->name : "none");
 #endif
     switch (hooknum){
@@ -32,29 +33,29 @@ static unsigned int firewall(unsigned int hooknum,
     }
 }
 
-/* This function initializes the hook_ops struct, sets the hooknum and hooks it to the firewall */
-static void hook_ops_default(struct nf_hook_ops *hook_ops, unsigned int hooknum){
+/* This function initializes the hook_ops struct, sets the hooknum and connects it to the firewall */
+static void hook_ops_init(struct nf_hook_ops *hook_ops, unsigned int hooknum){
     memset(hook_ops, 0, sizeof(struct nf_hook_ops));
     hook_ops->pf = PF_INET;
     hook_ops->hooknum = hooknum;
     hook_ops->priority = NF_IP_PRI_FIRST;
     hook_ops->hook = &firewall;
 #ifdef DEBUG
-    printk(KERN_INFO "hook %d initialized\n", hooknum);
+    printk(KERN_DEBUG "hook %d initialized\n", hooknum);
 #endif
 }
 
 static int __init hw1_init_function(void) {
 #ifdef DEBUG
-    printk(KERN_INFO "Initializing hooks...\n");
+    printk(KERN_DEBUG "Initializing hooks...\n");
 #endif
     /* initialize the hook option structs for the hook points defined by the exercise */
-    hook_ops_default(&hooks[0], NF_INET_FORWARD);
-    hook_ops_default(&hooks[1], NF_INET_LOCAL_IN);
-    hook_ops_default(&hooks[2], NF_INET_LOCAL_OUT);
+    hook_ops_init(&hooks[0], NF_INET_FORWARD);
+    hook_ops_init(&hooks[1], NF_INET_LOCAL_IN);
+    hook_ops_init(&hooks[2], NF_INET_LOCAL_OUT);
 
 #ifdef DEBUG
-    printk(KERN_INFO "Registering hooks...\n");
+    printk(KERN_DEBUG "Registering hooks...\n");
 #endif
     /* nf_register_hooks will register all the hooks and automatically unregister all of them if one fails */
     return nf_register_hooks(hooks, NUM_HOOKS);
@@ -62,11 +63,10 @@ static int __init hw1_init_function(void) {
 
 static void __exit hw1_exit_function(void) {
 #ifdef DEBUG
-    printk(KERN_INFO "Removing hooks...\n");
+    printk(KERN_DEBUG "Removing hooks...\n");
 #endif
     nf_unregister_hooks(hooks, NUM_HOOKS);
 }
 
 module_init(hw1_init_function);
 module_exit(hw1_exit_function);
-
