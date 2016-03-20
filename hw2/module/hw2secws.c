2061bb49e5b67b89410171e32b61c83dd501be0c
Tomer Brisker <tbrisker@gmail.com>
2016-03-20 21:57:23 +0200

    decouple firewall and intorduce sysfs

--- hw2/module/hw2secws.c
+++ hw2/module/hw2secws.c
@@ -3,9 +3,15 @@
 #include <linux/netdevice.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tomer Brisker");
 
+/************/
+/* Firewall */
+/************/
 #define NUM_HOOKS 3
 
 /* Array to hold our hook definitions so we can easily register and unregister them */
@@ -53,7 +59,7 @@ static void hook_ops_init(struct nf_hook_ops *hook_ops, unsigned int hooknum){
 #endif
 }
 
-static int __init hw1_init_function(void) {
+static int init_firewall(){
 #ifdef DEBUG
     printk(KERN_DEBUG "Initializing hooks...\n");
 #endif
@@ -69,12 +75,105 @@ static int __init hw1_init_function(void) {
     return nf_register_hooks(hooks, NUM_HOOKS);
 }
 
-static void __exit hw1_exit_function(void) {
+static void cleanup_firewall(){
 #ifdef DEBUG
     printk(KERN_DEBUG "Removing hooks...\n");
 #endif
     nf_unregister_hooks(hooks, NUM_HOOKS);
 }
 
-module_init(hw1_init_function);
-module_exit(hw1_exit_function);
+/***********/
+/*  Sysfs  */
+/***********/
+
+static int major_number;
+static struct class* sysfs_class = NULL;
+static struct device* sysfs_device = NULL;
+
+static unsigned int sysfs_int = 0;
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE
+};
+
+ssize_t display(struct device *dev, struct device_attribute *attr, char *buf)   //sysfs show implementation
+{
+    return scnprintf(buf, PAGE_SIZE, "%u\n", sysfs_int);
+}
+
+ssize_t modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)    //sysfs store implementation
+{
+    int temp;
+    if (sscanf(buf, "%u", &temp) == 1)
+        sysfs_int = temp;
+    return count;
+}
+
+static DEVICE_ATTR(sysfs_att, S_IRWXO , display, modify);
+
+static int init_sysfs(void)
+{
+    //create char device
+    major_number = register_chrdev(0, "Sysfs_Device", &fops);\
+    if (major_number < 0)
+        return -1;
+
+    //create sysfs class
+    sysfs_class = class_create(THIS_MODULE, "Sysfs_class");
+    if (IS_ERR(sysfs_class))
+    {
+        unregister_chrdev(major_number, "Sysfs_Device");
+        return -1;
+    }
+
+    //create sysfs device
+    sysfs_device = device_create(sysfs_class, NULL, MKDEV(major_number, 0), NULL, "sysfs_class" "_" "sysfs_Device");
+    if (IS_ERR(sysfs_device))
+    {
+        class_destroy(sysfs_class);
+        unregister_chrdev(major_number, "Sysfs_Device");
+        return -1;
+    }
+
+    //create sysfs file attributes
+    if (device_create_file(sysfs_device, (const struct device_attribute *)&dev_attr_sysfs_att.attr))
+    {
+        device_destroy(sysfs_class, MKDEV(major_number, 0));
+        class_destroy(sysfs_class);
+        unregister_chrdev(major_number, "Sysfs_Device");
+        return -1;
+    }
+
+    return 0;
+}
+
+static void cleanup_sysfs(void)
+{
+    device_remove_file(sysfs_device, (const struct device_attribute *)&dev_attr_sysfs_att.attr);
+    device_destroy(sysfs_class, MKDEV(major_number, 0));
+    class_destroy(sysfs_class);
+    unregister_chrdev(major_number, "Sysfs_Device");
+}
+
+/**********/
+/*  CORE  */
+/**********/
+static int __init hw2_init_function(void) {
+    int err = init_firewall();
+    if (err)
+        return err;
+    err = init_sysfs();
+    if (err){
+        cleanup_firewall();
+        return err;
+    }
+    return 0;
+}
+
+static void __exit hw2_exit_function(void) {
+    cleanup_sysfs();
+    cleanup_firewall();
+}
+
+module_init(hw2_init_function);
+module_exit(hw2_exit_function);
