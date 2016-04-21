9a18ee1c43a2141e8408546e64ae771a52edc04f
Tomer Brisker <tbrisker@gmail.com>
2016-04-21 22:38:31 +0300

    rename sysfs to stats

--- firewall/fw.c
+++ firewall/fw.c
@@ -16,19 +16,19 @@ static int __init firewall_init_function(void) {
 #ifdef DEBUG
     printk(KERN_DEBUG "Firewall initialized successfully!\n");
 #endif
-    if ((err = init_sysfs())){
-        printk(KERN_ERR "sysfs interface init failed with error %d!\n", err);
+    if ((err = init_stats())){
+        printk(KERN_ERR "stats interface init failed with error %d!\n", err);
         cleanup_firewall(); //we already initialized the fw, so we need to clean it up
         return err;
     }
 #ifdef DEBUG
-    printk(KERN_DEBUG "sysfs interface initialized successfully!\n");
+    printk(KERN_DEBUG "stats interface initialized successfully!\n");
 #endif
     return 0;
 }
 
 static void __exit firewall_exit_function(void) {
-    cleanup_sysfs(3);
+    cleanup_stats(3);
     cleanup_firewall();
 }
 
