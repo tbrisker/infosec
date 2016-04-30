ec6a8034c242557e2d70e662ddb586a2376ccd80
Tomer Brisker <tbrisker@gmail.com>
2016-04-30 21:08:30 +0300

    split parsing to util.c

--- interface/main.c
+++ interface/main.c
@@ -1,68 +1,7 @@
 #include "main.h"
 
-/* get an int from a file or -1 on error */
-int read_int(char * path){
-    int val = -1;
-    FILE * fp;
-    fp = fopen(path, "r");
-    if (!fp){
-        perror("Error opening file");
-        return val;
-    }
-    if (fscanf(fp, "%d", &val) != 1){
-        perror("Error reading file");
-    }
-    fclose(fp);
-    return val;
-}
-
-/* write a char to a file */
-void write_char(char * path, char c){
-    FILE * fp;
-    fp = fopen(path, "w");
-    if (!fp){
-        perror("Error opening file");
-        return;
-    }
-    if (fprintf(fp, "%c", c) < 0){
-        perror("Error writing file");
-    }
-    fclose(fp);
-}
-
-//convert
-char * ip_to_s(int ip){
-    struct in_addr addr = {ip};
-    return inet_ntoa(addr);
-}
-
-char time_str[21];
-char * time_to_s(long timestamp){
-    struct tm* parsed = localtime(&timestamp);
-    if (strftime(time_str, 20, "%d/%m/%Y %T", parsed))
-        return time_str;
-    return "Error";
-}
-
-char * prot_to_s(int protocol){
-    switch (protocol){
-    case PROT_ICMP:
-        return "icmp";
-    case PROT_TCP:
-        return "tcp";
-    case PROT_UDP:
-        return "udp";
-    default:
-        return "other";
-    }
-}
-
-char * action_to_s(char action){
-    return action ? "accept" : "drop";
-}
-
 void print_log_row(log_row_t row){
-    char src_ip[16], dst_ip[16];
+    char src_ip[16], dst_ip[16]; // max ip length: 4*3+3*1=15
     inet_ntop(AF_INET, &row.src_ip, src_ip, 16);
     inet_ntop(AF_INET, &row.dst_ip, dst_ip, 16);
     printf("%s\t%-15s\t%-15s\t%hu\t\t%hu\t\t%s\t\t%hhu\t%s\t%hu\t%u\n",
@@ -74,30 +13,85 @@ void print_log_row(log_row_t row){
 
 void show_log(void){
     int fd;
-    fd = open(DEV_PATH(log), O_RDONLY);
+    fd = open(DEV_PATH("log"), O_RDONLY);
     if (fd<0){
         perror("Error opening file");
         return;
     }
     printf("timestamp\t\tsrc_ip\t\tdst_ip\t\tsrc_port\tdst_port\tprotocol\thooknum\taction\treason\tcount\n");
     log_row_t row;
-    while (read(fd, &row, sizeof(log_row_t))== sizeof(log_row_t)) {
+    while (read(fd, &row, sizeof(log_row_t)) == sizeof(log_row_t)) {
         print_log_row(row);
     }
     close(fd);
 }
+loopback any 127.0.0.1/8 127.0.0.1/8 any any any any accept
+
+void print_rule(rule_t rule){
+    printf("%s %s %s %s %s %s %s %s %s\n",
+        rule.name, dir_to_s(rule.direction));
+}
+
+void show_rules(int count){
+    int fd,i;
+    rule_t rules[count];
+    fd = open(DEV_PATH("rules"), O_RDONLY);
+    if (fd<0){
+        perror("Error opening file");
+        return;
+    }
+    if (read(fd, &rules, sizeof(rule_t)*count) == sizeof(rule_t)*count) {
+        for (i = 0; i < count; ++i)
+            print_rule(rules[i]);
+    }
+    close(fd);
+}
+
+void load_rules(const char * path){
+
+}
 
 int main(int argc, char const *argv[]){
-    // if (argc > 3 || argc == 1){
-    //     printf("Invalid number of arguments.\n");
-    //     return -1;
-    // }
-    // if (!strcmp(argv[1], "show_log")){
-    //     show_log();
-    //     return 0;
-    // }
-    // printf("Invalid argument.\n");
-    // return -1;
-    show_log();
+    int count;
+    int ip, mask;
+    char str[] = "192.168.1.1/24";
+    mask = s_to_ip_and_mask(str, &ip);
+    printf("%d/%d\n", ip, mask);
+    if (argc > 3 || argc == 1){
+        printf("Invalid number of arguments.\n");
+        return -1;
+    }
+    if (!strcmp(argv[1], "activate")){
+        write_char(SYSFS_PATH("fw_rules/active"), "1");
+        return 0;
+    }
+    if (!strcmp(argv[1], "deactivate")){
+        write_char(SYSFS_PATH("fw_rules/active"), "0");
+        return 0;
+    }
+    if (!strcmp(argv[1], "show_rules")){
+        count = read_int(SYSFS_PATH("fw_rules/rules_size"));
+        if (count > 0)
+            show_rules(count);
+        return 0;
+    }
+    if (!strcmp(argv[1], "clear_rules")){
+        write_char(SYSFS_PATH("fw_rules/rules_clear"), "1");
+        return 0;
+    }
+    if (!strcmp(argv[1], "load_rules") && argc == 2){
+        load_rules(argv[2]);
+        return 0;
+    }
+    if (!strcmp(argv[1], "show_log")){
+        show_log();
+        return 0;
+    }
+    if (!strcmp(argv[1], "clear_log")){
+        write_char(SYSFS_PATH("fw_log/log_clear"), "1");
+        return 0;
+    }
+    printf("Invalid argument.\n");
+    return -1;
     return 0;
 }
---
+++ interface/util.c
@@ -0,0 +1,171 @@
+#include "main.h"
+
+/**************/
+/* File utils */
+/**************/
+
+/* get an int from a file or -1 on error */
+/* we take advantage of the stdlib here to parse the data */
+int read_int(char * path){
+    int val = -1;
+    FILE * fp;
+    fp = fopen(path, "r");
+    if (!fp){
+        perror("Error opening file");
+        return val;
+    }
+    if (fscanf(fp, "%d", &val) != 1){
+        perror("Error reading file");
+    }
+    fclose(fp);
+    return val;
+}
+
+/* write a char to a file */
+void write_char(char *path, const char *c){
+    int fd;
+    fd = open(path, O_WRONLY);
+    if (fd<0){
+        perror("Error opening file");
+        return;
+    }
+    if (write(fd, c, 1) != 1){
+        perror("Error writing file");
+    }
+    close(fd);
+}
+
+
+/********************/
+/* Conversion utils */
+/********************/
+
+char time_str[21];
+char * time_to_s(long timestamp){
+    struct tm* parsed = localtime(&timestamp);
+    if (strftime(time_str, 20, "%d/%m/%Y %T", parsed))
+        return time_str;
+    return "Error";
+}
+
+char * prot_to_s(int protocol){
+    switch (protocol){
+    case PROT_ICMP:
+        return "icmp";
+    case PROT_TCP:
+        return "tcp";
+    case PROT_UDP:
+        return "udp";
+    default:
+        return "other";
+    }
+}
+
+int s_to_prot(char * str){
+    if (!strcmp(str, "icmp"))
+        return PROT_ICMP;
+    if (!strcmp(str, "tcp"))
+        return PROT_TCP;
+    if (!strcmp(str, "udp"))
+        return PROT_UDP;
+    if (!strcmp(str, "any"))
+        return PROT_ANY;
+    if (!strcmp(str, "other"))
+        return PROT_OTHER;
+    printf("Invalid protocol %s!\n", str);
+    return -1;
+}
+
+char * action_to_s(char action){
+    return action ? "accept" : "drop";
+}
+
+int s_to_action(char * str){
+    if (!strcmp(str, "accept"))
+        return NF_ACCEPT;
+    if (!strcmp(str, "drop"))
+        return NF_DROP;
+    printf("Invalid action %s!", str);
+    return -1;
+}
+
+char * dir_to_s(int dir){
+    switch(dir){
+    case DIRECTION_IN:
+        return "in";
+    case DIRECTION_OUT:
+        return "out";
+    case DIRECTION_ANY:
+        return "any";
+    }
+    return "ERR";
+}
+
+int s_to_dir(char *str){
+    if (!strcmp(str, "in"))
+        return DIRECTION_IN;
+    if (!strcmp(str, "out"))
+        return DIRECTION_OUT;
+    if (!strcmp(str, "any"))
+        return DIRECTION_ANY;
+    printf("Invalid action %s!", str);
+    return -1;
+}
+
+/* Convert a string to ip and mask */
+int s_to_ip_and_mask(char *str, int *ip){
+    struct in_addr addr = {0};
+    char *nps, *strcopy;
+    int mask = 0;
+    if (!strcmp(str, "any")){
+        *ip = 0; // 0.0.0.0 will be used to denote "any" ip as is common.
+        return 0;
+    }
+    nps = strchr(str, '/');
+    if (nps != NULL){
+        *nps = '\0';  //split the nps from the ip so they can be parsed seperately
+        nps++;
+        if (sscanf(nps, "%d", &mask) != 1 || mask > 32 || mask < 0){
+            printf("Invalid ip %s/%s\n", str, nps);
+            return -1;
+        }
+    }
+    if (!inet_aton(str, &addr)){
+        printf("Invalid ip %s\n", str);
+        return -1;
+    }
+    *ip = addr.s_addr;
+    return mask;
+}
+
+char ack_to_chr(char *str){
+    if (!strcmp(str, "yes"))
+        return ACK_YES;
+    if (!strcmp(str, "no"))
+        return ACK_NO;
+    if (!strcmp(str, "any"))
+        return ACK_ANY;
+    printf("Invalid ack %s!", str);
+    return -1;
+}
+
+char reason_str[5];
+char * reason_to_s(int reason){
+    switch (reason){
+    case REASON_FW_INACTIVE:
+        return "REASON_FW_INACTIVE";
+        break;
+    case REASON_NO_MATCHING_RULE:
+        return "REASON_NO_MATCHING_RULE";
+        break;
+    case REASON_XMAS_PACKET:
+        return "REASON_XMAS_PACKET";
+        break;
+    case REASON_ILLEGAL_VALUE:
+        return "REASON_ILLEGAL_VALUE";
+        break;
+    default:
+        snprintf(reason_str, 5, "%d", reason);
+    }
+    return reason_str;
+}
