0a4af98d2fbb3eb596cfbfe6dd5d236765375908
Tomer Brisker <tbrisker@gmail.com>
2016-05-27 18:13:07 +0300

    tcp state machine

--- firewall/fw_conn_tab.c
+++ firewall/fw_conn_tab.c
@@ -15,7 +15,7 @@ static connection * find_connection(__be32 src_ip, __be16 src_port, __be32 dst_i
     connection *cur, *tmp;
     unsigned long expiry = get_seconds() - TIMEOUT; //timestamp for expiring old connections
     list_for_each_entry_safe(cur, tmp, &conn_table, list){
-        if (cur->timestamp < expiry){
+        if (cur->timestamp < expiry || cur->src_state == C_CLOSED || cur->dst_state == C_CLOSED){
             del_con(cur);
             continue;
         }
@@ -31,16 +31,90 @@ static connection * find_connection(__be32 src_ip, __be16 src_port, __be32 dst_i
 /* check if a given connection is permitted in the connection table
  * and update the connection state if it changed.
  * Sets the action on the packet according to the decision.
+ * Note: this function assumes that tcp_header->ack is true.
  */
-void check_conn_tab(rule_t *pkt, struct tcphdr *tcp_header){
-    connection *con;
-    con = find_connection(pkt->src_ip, pkt->src_port, pkt->dst_ip, pkt->dst_port);
+reason_t check_conn_tab(rule_t *pkt, struct tcphdr *tcp_header){
+    int reverse; //is this packet in the original direction or reversed?
+    connection *con = find_connection(pkt->src_ip, pkt->src_port, pkt->dst_ip, pkt->dst_port);
     if (NULL == con){ //non existing connection - drop the packet
         pkt->action = NF_DROP;
-        return;
+        return REASON_CONN_NOT_EXIST;
     }
-    pkt->action = NF_ACCEPT;
-    return;
+    pkt->action = NF_ACCEPT; //existing connection - default to accept
+    reverse = (pkt->src_ip == con->dst_ip && pkt->src_port == con->dst_port);
+
+    //in handshake
+    if (con->src_state == C_SYN_SENT){
+        //handshake stage 2
+        if (reverse && con->dst_state == C_LISTEN && tcp_header->syn){
+            con->dst_state = C_SYN_RECEIVED;
+            return REASON_CONN_EXIST;
+        }
+        //handshake stage 3
+        if (!reverse && con->dst_state == C_SYN_RECEIVED && !tcp_header->syn){
+            con->src_state = con->dst_state = C_ESTABLISHED;
+            return REASON_CONN_EXIST;
+        }
+        // any packet that doesn't match the handshake protocol is invalid.
+        pkt->action = NF_DROP;
+        return REASON_TCP_NON_COMPLIANT;
+    }
+    if (tcp_header->syn){ //syn is valid only during handshake, drop otherwise
+        pkt->action = NF_DROP;
+        return REASON_TCP_NON_COMPLIANT;
+    }
+
+    // in established connection
+    if (con->src_state == C_ESTABLISHED) {
+        if (tcp_header->fin){ //handle close requests
+            if (reverse){ //the server requested the close
+                con->src_state = C_CLOSE_WAIT;
+                con->dst_state = C_FIN_WAIT_1;
+            } else { //the client requested the close
+                con->src_state = C_FIN_WAIT_1;
+                con->dst_state = C_CLOSE_WAIT;
+            }
+        }
+        //any packet is valid now (we already made sure syn=0, ack=1)
+        return REASON_CONN_EXIST;
+    }
+
+    //in closing handshake
+    if (reverse){
+        if (con->src_state == C_FIN_WAIT_1){
+            if (tcp_header->fin){
+                con->src_state = C_TIME_WAIT;
+                con->dst_state = C_LAST_ACK;
+            } else {
+                con->src_state = C_FIN_WAIT_2
+            }
+        } else if (con->src_state == C_FIN_WAIT_2) {
+            if (tcp_header->fin){
+                con->src_state = C_TIME_WAIT;
+                con->dst_state = C_LAST_ACK;
+            }
+        } else if (con->src_state == C_LAST_ACK) {
+            con->src_state = C_CLOSED
+        }
+    } else {
+        if (con->dst_state == C_FIN_WAIT_1){
+            if (tcp_header->fin){
+                con->dst_state = C_TIME_WAIT;
+                con->src_state = C_LAST_ACK;
+            } else {
+                con->dst_state = C_FIN_WAIT_2
+            }
+        } else if (con->dst_state == C_FIN_WAIT_2) {
+            if (tcp_header->fin){
+                con->dst_state = C_TIME_WAIT;
+                con->src_state = C_LAST_ACK;
+            }
+        } else if (con->dst_state == C_LAST_ACK) {
+            con->dst_state = C_CLOSED
+        }
+    }
+
+    return REASON_CONN_EXIST;
 }
 
 /* Add a new connection to the connection table */
@@ -55,7 +129,7 @@ void new_connection(rule_t pkt){
     con->src_port  = pkt.src_port;
     con->dst_ip    = pkt.dst_ip;
     con->dst_port  = pkt.dst_port;
-    con->src_state = C_SYN_SENT;
+    con->src_state = C_SYN_SENT; //handshake stage 1
     con->dst_state = C_LISTEN; //assume the server is listening - will timeout if not
     list_add(&con->list, &conn_table);
 }
